6c86057f9e57d200a7c56eaa1083edc155952b02
Chun Kit Chan <tommychan19910123@gmail.com>
2014-07-11 17:10:20 +0800

    Add Kinect Chooser and Kinect Region

--- SignLanguageEducationSystem/MainWindow.xaml.cs
+++ SignLanguageEducationSystem/MainWindow.xaml.cs
@@ -12,14 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
 
 namespace SignLanguageEducationSystem {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		private KinectSensorChooser sensorChooser;
+
 		public MainWindow() {
 			InitializeComponent();
+
+			Loaded += MainWindow_Loaded;
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+			this.sensorChooser = new KinectSensorChooser();
+			this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+			this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
+			this.sensorChooser.Start(); 
+		}
+
+		private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args) {
+
+			bool error = false;
+
+			if (args.OldSensor != null) {
+				try {
+					args.OldSensor.DepthStream.Range = DepthRange.Default;
+					args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+					args.OldSensor.DepthStream.Disable();
+					args.OldSensor.ColorStream.Disable();
+				} catch (InvalidOperationException) { error = true; }
+			}
+
+			if (args.NewSensor != null) {
+				try {
+					args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+					args.NewSensor.SkeletonStream.Enable();
+
+					try {
+						args.NewSensor.DepthStream.Range = DepthRange.Near;
+						args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+						args.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+					} catch (InvalidOperationException) {
+						// Switch back to normal mode if Kinect does not support near mode
+						args.NewSensor.DepthStream.Range = DepthRange.Default;
+						args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+						error = true;
+					}
+				} catch (InvalidOperationException) { error = true; }
+			}
+
+			if (!error) {
+				this.kinectRegion.KinectSensor = args.NewSensor;
+			}
 		}
 	}
 }
